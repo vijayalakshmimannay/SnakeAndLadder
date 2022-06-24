20eb37d44f23aadebf5f3aedc04f119a2b9679fd
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-24 09:18:11 +0530

    [vijayalakshmi] check for dice option

--- SnakeAndLadder/SnakeandLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeandLadder/SnakeAndLadder.cs
@@ -9,10 +9,27 @@ namespace SnakeandLadder
 {
     internal class SnakeAndLadder
     {
-        int startPosition = 0, playerPosition = 0;
-        public void Position()
+        public int startPosition = 0, playerPosition = 0;
+        const int noPlay = 0, snake = 1, ladder = 2;
+        Random random = new Random();
+        public void diceRoll()
         {
-            Console.WriteLine("Initial position of player is " + playerPosition);
+            int diceNo = random.Next(1, 6);
+            switch (random.Next(0, 3))
+            {
+                case noPlay:
+                    playerPosition += 0;
+                    break;
+                case snake:
+                    playerPosition -= diceNo;
+                    break;
+                case ladder:
+                    playerPosition += diceNo;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Dice value of player is {0} with Position at {1}", diceNo, playerPosition);
         }
     }
 }
